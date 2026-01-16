6956942f8e76c887b5fdc3348198db9ec5b245c1
Ammaar Shareef <ammhood1602@gmail.com>
2026-01-15 21:16:24 -0500

    Add frontend as normal folder (not submodule)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState("")
+
+  const getRoomCode = async () => {
+    const res = await fetch("http://localhost:3001/room-code");
+    const roomCode = await res.text();
+    setCode(roomCode)
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={getRoomCode}>
+           Generate Room Code
+        </button>
+        <p>
+          The Room code is {code}
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
